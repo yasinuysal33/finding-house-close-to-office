4a49be91e65e2a8b7f1977d19f4fe5315954df79
yasinuysal33 <uysal.e.yasin@gmail.com>
2022-10-16 13:48:14 +0300

    startup

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,5 @@ import { useLoadScript } from "@react-google-maps/api";
 import Map from "../components/map";
 
 export default function Home() {
-  console.log("deneme");
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    libraries: ["places"],
-  });
-
-  if (!isLoaded) return <div>Loading...</div>;
-  return <Map />;
+  return <div>Map</div>;
 }
