dccfb8226cca49dc7b4004c4c8dab366790219f2
yasinuysal33 <uysal.e.yasin@gmail.com>
2022-10-26 22:57:19 +0300

    En localization

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
     libraries: ["places"],
+    language:"en"
   });
   if (!isLoaded) return <div>Loading...</div>;
   return <Map />;
